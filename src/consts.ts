ce219a4372ab97cf2c9c58230961b96b87faea46
Manuel Denis <manueldenis.developer@gmail.com>
2024-08-28 18:06:31 +0200

    [AÑADIENDO] Servicios y MetaTags

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "Alejandro Escobar";
+export const SITE_TAGLINE =
+  "Coaching para emprendedores 🚀 De Emprendedor a Empresario 🚀 🌟Transforma tu negocio ¡YA! 🌟 Sesiones 1:1 totalmente individualizadas y seguimiento continuo"; // site tagline
+export const SITE_DESCRIPTION =
+  "Coaching para emprendedores 🚀 De Emprendedor a Empresario 🚀 🌟Transforma tu negocio ¡YA! 🌟 Sesiones 1:1 totalmente individualizadas y seguimiento continuo"; // site meta description for SEO
---
+++ src/lib/HeaderLinks.ts
@@ -0,0 +1,18 @@
+export const HeaderLinks = [
+  {
+    label: "Sobre mi",
+    href: "/#sobre-mi",
+  },
+  {
+    label: "Servicios",
+    href: "/#servicios",
+  },
+  {
+    label: "Metodologia",
+    href: "/#metodologia",
+  },
+  {
+    label: "Testimonios",
+    href: "/#testimonios",
+  },
+];
