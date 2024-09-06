6debcfbf4cedd16a50d074d3396f33e3a2c1acbc
Manuel Denis <manueldenis.developer@gmail.com>
2024-09-07 01:16:59 +0200

    [AÑADIENDO] Nueva imagen

---
+++ src/lib/SectionMetodologia.ts
@@ -0,0 +1,35 @@
+export const SectionMetodologiaTitulo = 'Metodologia'
+
+export const SectionMetodologiaDescripcion =
+  'Metodología de trabajo para acompañarte en el proceso de creación y desarrollo de tu negocio.'
+
+export const SectionMetodologiaItems = [
+  {
+    titulo: 'Desafios',
+    descripcion: 'Identificamos los desafíos que enfrentas a diario y analizamos la situación actual del negocio.',
+
+    icono: 'round-looks-one'
+  },
+  {
+    titulo: 'Opciones',
+    descripcion:
+      'Analizamos las opciones disponibles y definimos un plan de acción personalizado para impulsar tu negocio.',
+    icono: 'round-looks-two'
+  },
+  {
+    titulo: 'Consecuencias',
+    descripcion:
+      'Analizamos las consecuencias de cada opción y definimos un plan de acción personalizado para impulsar tu negocio.',
+    icono: 'round-looks-three'
+  },
+  {
+    titulo: 'Soluciones creativas',
+    descripcion: 'Buscamos soluciones creativas y definimos un plan de acción personalizado para impulsar tu negocio.',
+    icono: 'round-looks-four'
+  },
+  {
+    titulo: 'Conclusiones',
+    descripcion: 'Analizamos las conclusiones y definimos un plan de acción personalizado para impulsar tu negocio.',
+    icono: 'round-looks-five'
+  }
+]
